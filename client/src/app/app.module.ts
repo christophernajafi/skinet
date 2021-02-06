bdea0498db42b4d58d4a3ea6f78342852389d248
Christopher Najafi <chris.najafi@gmail.com>
2021-02-06 11:10:13 -0500

    Added routing, the product details component, and lazy loading of angular modules.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HomeModule } from './home/home.module';
 import { ShopModule } from './shop/shop.module';
 import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
@@ -14,7 +15,7 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HttpClientModule,
     CoreModule,
-    ShopModule,
+    HomeModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
