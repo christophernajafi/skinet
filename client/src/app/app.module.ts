a463e4a48c05aeb66777ec875e88a4eaae407eeb
Christopher Najafi <chris.najafi@gmail.com>
2021-02-15 09:50:15 -0500

    Added checkout and order features to client.

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { HomeModule } from './home/home.module';
 import { CoreModule } from './core/core.module';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule } from '@angular/core';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,10 +23,10 @@ import { AppComponent } from './app.component';
     BrowserAnimationsModule,
     NgxSpinnerModule,
   ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: LoadingInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
   ],
   bootstrap: [AppComponent],
 })
--- client/src/app/checkout/checkout.module.ts
+++ client/src/app/checkout/checkout.module.ts
@@ -1,10 +1,16 @@
+import { SharedModule } from './../shared/shared.module';
 import { CheckoutRoutingModule } from './checkout-routing.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CheckoutComponent } from './checkout.component';
+import { CheckoutAddressComponent } from './checkout-address/checkout-address.component';
+import { CheckoutDeliveryComponent } from './checkout-delivery/checkout-delivery.component';
+import { CheckoutPaymentComponent } from './checkout-payment/checkout-payment.component';
+import { CheckoutReviewComponent } from './checkout-review/checkout-review.component';
+import { CheckoutSuccessComponent } from './checkout-success/checkout-success.component';
 
 @NgModule({
-  declarations: [CheckoutComponent],
-  imports: [CommonModule, CheckoutRoutingModule],
+  declarations: [CheckoutComponent, CheckoutAddressComponent, CheckoutDeliveryComponent, CheckoutPaymentComponent, CheckoutReviewComponent, CheckoutSuccessComponent],
+  imports: [CommonModule, CheckoutRoutingModule, SharedModule],
 })
 export class CheckoutModule {}
